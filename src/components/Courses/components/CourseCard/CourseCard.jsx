e65828dc97cd0b84a50f54f58bd91a3b9bf058e1
Anastasia <nastia.shrai@gmail.com>
2022-05-11 23:09:14 +0300

    feat: add CourseCard component

---
+++ src/components/Courses/components/CourseCard/CourseCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Button from '../../../../common/Button/Button';
+import '../../../../common/Button/Button.css';
+import './CourseCard.css';
+
+function CourseCard(props) {
+	let hours = Math.floor(props.duration / 60);
+	let minutes = props.duration % 60;
+
+	return (
+		<div className='card__container'>
+			<div className='card__content'>
+				<h2 className='card__title'>{props.title}</h2>
+				<p className='card__description'>{props.description}</p>
+			</div>
+			<div className='card__details'>
+				<p className='card__info'>
+					<strong>Authors: </strong> {props.author}
+				</p>
+				<p className='card__info'>
+					<strong>Duration: </strong>
+					{hours}:{minutes} hours
+				</p>
+				<p className='card__info'>
+					<strong>Created: </strong>
+					{props.creationDate}
+				</p>
+				<Button className='showCourse' buttonText='Show course' />
+			</div>
+		</div>
+	);
+}
+
+export default CourseCard;

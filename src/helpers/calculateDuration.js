e585456ea33d887192f9027109adbbe3a502f645
Anastasia <nastia.shrai@gmail.com>
2022-06-06 14:42:20 +0300

    fix: correct calculateDuration function

--- src/helpers/calculateDuration.js
+++ src/helpers/calculateDuration.js
@@ -1,13 +1,17 @@
 const calculateDuration = (value) => {
-	let hours = Math.floor(value / 60);
-	let minutes = value % 60;
-	if (hours < 10) {
-		hours = `0${hours}`;
+	if (!value) {
+		return '00:00';
+	} else {
+		let hours = Math.floor(value / 60);
+		let minutes = value % 60;
+		if (hours < 10) {
+			hours = `0${hours}`;
+		}
+		if (minutes < 10) {
+			minutes = `0${minutes}`;
+		}
+		return `${hours}:${minutes}`;
 	}
-	if (minutes < 10) {
-		minutes = `0${minutes}`;
-	}
-	return `${hours}:${minutes}`;
 };
 
 export default calculateDuration;

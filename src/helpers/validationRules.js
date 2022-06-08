95a5c2e9da522f86c4730e2974fda5ed97ba7778
Anastasia <nastia.shrai@gmail.com>
2022-06-08 21:06:44 +0300

    refactor: move validationRules to helpers

---
+++ src/helpers/validationRules.js
@@ -0,0 +1,27 @@
+export const validationRules = {
+	title: {
+		required: {
+			value: true,
+			message: 'The title is required',
+		},
+	},
+	description: {
+		required: {
+			value: true,
+			message: 'This field is required',
+		},
+		custom: {
+			isValid: (value) => {
+				if (!value) return false;
+				return value.length < 2 ? false : true;
+			},
+			message: 'The description needs to be at least 2 characters long',
+		},
+	},
+	duration: {
+		custom: {
+			isValid: (value) => parseInt(value, 10) > 0,
+			message: 'The duration needs to be more than 0',
+		},
+	},
+};

303dcb48c1592b3552c55ce2c95f8430ba5fe2a5
Anastasia <nastia.shrai@gmail.com>
2022-06-19 17:07:17 +0300

    feat: implement logout function and add routing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,49 @@ import React from 'react';
 import './Header.css';
 import Logo from './components/Logo/Logo.jsx';
 import Button from '../../common/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import axios from '../../api/axios';
+
+const LOGOUT_URL = '/logout';
 
 const Header = () => {
+	const navigate = useNavigate();
+	const handleLogout = (e) => {
+		e.preventDefault();
+		axios
+			.delete(LOGOUT_URL, {
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `${localStorage.getItem('token')}`,
+				},
+			})
+			.then(() => {
+				localStorage.clear();
+				localStorage.removeItem('token');
+				localStorage.removeItem('name');
+				navigate('/login');
+			})
+			.catch((error) => {
+				alert(`${error.message}`);
+			});
+	};
+
 	return (
 		<header className='header'>
 			<Logo />
 			<div className='header__content'>
-				<h4 className='header__user'>Stacey</h4>
-				<Button className='login' buttonText='Logout' />
+				{localStorage.getItem('token') && localStorage.getItem('name') ? (
+					<>
+						<h4 className='header__user'>{localStorage.getItem('name')}</h4>
+						<Button
+							className='login-btn'
+							buttonText='Logout'
+							handleClick={handleLogout}
+						/>
+					</>
+				) : (
+					''
+				)}
 			</div>
 		</header>
 	);

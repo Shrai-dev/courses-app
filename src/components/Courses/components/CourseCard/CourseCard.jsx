98f0d90bcd66df6d4b04d766c4b162f8b91bd059
Anastasia <nastia.shrai@gmail.com>
2022-06-19 16:52:06 +0300

    fix: change handling props and add routing

--- src/components/Courses/components/CourseCard/CourseCard.jsx
+++ src/components/Courses/components/CourseCard/CourseCard.jsx
@@ -1,30 +1,43 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../../../common/Button/Button';
 import '../../../../common/Button/Button.css';
 import './CourseCard.css';
 import calculateDuration from '../../../../helpers/calculateDuration';
 import formatDate from '../../../../helpers/formatDate';
+import getAuthorName from '../../../../helpers/getAuthorName';
 
-const CourseCard = (props) => {
+const CourseCard = (course) => {
+	const navigate = useNavigate();
+
+	const openCourse = (id) => {
+		navigate(`/courses/${id}`);
+	};
+
 	return (
 		<div className='card__container'>
 			<div className='card__content'>
-				<h2 className='card__title'>{props.title}</h2>
-				<p className='card__description'>{props.description}</p>
+				<h2 className='card__title'>{course.course.title}</h2>
+				<p className='card__description'>{course.course.description}</p>
 			</div>
 			<div className='card__details'>
 				<p className='card__info card__author'>
-					<strong>Authors: </strong> {props.author}
+					<strong>Authors: </strong> {getAuthorName(course.course.authors)}
 				</p>
 				<p className='card__info'>
 					<strong>Duration: </strong>
-					{calculateDuration(props.duration)} hours
+					{calculateDuration(course.course.duration)} hours
 				</p>
 				<p className='card__info'>
 					<strong>Created: </strong>
-					{formatDate(props.creationDate)}
+					{formatDate(course.course.creationDate)}
 				</p>
-				<Button className='showCourse' buttonText='Show course' type='button' />
+				<Button
+					handleClick={() => openCourse(course.course.id)}
+					className='showCourse'
+					buttonText='Show course'
+					type='button'
+				/>
 			</div>
 		</div>
 	);
